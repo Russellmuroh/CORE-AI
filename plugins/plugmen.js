17f5630f49d9b0d324bf0230a16bbb82c679b312
PRO-DEVELOPER-1 <ndisaster20@gmail.com>
2025-04-01 07:44:44 +0300

    @bera

--- plugins/plugmen.js
+++ plugins/plugmen.js
@@ -1,17 +1,39 @@
-import moment from 'moment-timezone'; import fs from 'fs'; import os from 'os'; import pkg from '@whiskeysockets/baileys'; const { generateWAMessageFromContent, proto } = pkg; import config from '../config.cjs'; import axios from 'axios';
+import moment from 'moment-timezone'; 
+import fs from 'fs'; 
+import os from 'os'; 
+import pkg from '@whiskeysockets/baileys'; 
+const { generateWAMessageFromContent, proto } = pkg; 
+import config from '../config.cjs'; 
+import axios from 'axios';
 
 const menu = async (m, Matrix) => { const cmd = m.body.toLowerCase().trim(); const mode = config.MODE === 'public' ? 'public' : 'private';
 
 if (cmd === 'menu1') {
     const mainMenu = `
 
-╭━━━〔 ${config.BOT_NAME} 〕━━━┈⊷ ┃★ Owner : ${config.OWNER_NAME} ┃★ User : ${m.pushName} ┃★ Mode : ${mode} ┃★ Platform : ${os.platform()} ┃★ Version : 3.1.0 ╰━━━━━━━━━━━━━━━┈⊷
+╭━━━〔 ${config.BOT_NAME} 〕━━━┈⊷ 
+┃★ Owner : ${config.OWNER_NAME} 
+┃★ User : ${m.pushName} 
+┃★ Mode : ${mode} 
+┃★ Platform : ${os.platform()} 
+┃★ Version : 3.1.0 
+╰━━━━━━━━━━━━━━━┈⊷
 
 > Welcome ${m.pushName}!
 
 
 
-╭━━〔 Menu List 〕━━┈⊷ ┃◈ 1. Download Menu ┃◈ 2. Converter Menu ┃◈ 3. AI Menu ┃◈ 4. Tools Menu ┃◈ 5. Group Menu ┃◈ 6. Search Menu ┃◈ 7. Main Menu ┃◈ 8. Owner Menu ┃◈ 9. Stalk Menu ┃◈ update ╰──────────────┈⊷
+╭━━〔 Menu List 〕━━┈⊷ 
+┃◈ 1. Download Menu 
+┃◈ 2. Converter Menu 
+┃◈ 3. AI Menu 
+┃◈ 4. Tools Menu 
+┃◈ 5. Group Menu 
+┃◈ 6. Search Menu 
+┃◈ 7. Main Menu 
+┃◈ 8. Owner Menu 
+┃◈ 9. Stalk Menu 
+╰──────────────┈⊷
 
 > Reply with the number (1-9)`;
 
@@ -49,15 +71,97 @@ const getMenuImage = async () => {
         if (receivedMessage.message.extendedTextMessage.contextInfo?.stanzaId !== sentMessage.key.id) return;
 
         const menus = {
-            "1": "╭━━〔 Download Menu 〕━━┈⊷\n┃◈ apk\n┃◈ facebook\n┃◈ mediafire\n┃◈ pinterestdl\n┃◈ insta\n┃◈ ytmp3\n┃◈ ytmp4\n┃◈ play\n┃◈ song\n┃◈ video\n╰──────────────┈⊷",
-            "2": "╭━━〔 Converter Menu 〕━━┈⊷\n┃◈ attp\n┃◈ ebinary\n┃◈ dbinary\n┃◈ emojimix\n┃◈ mp3\n╰──────────────┈⊷",
-            "3": "╭━━〔 AI Menu 〕━━┈⊷\n┃◈ ai\n┃◈ bug\n┃◈ report\n┃◈ gpt\n┃◈ dalle\n┃◈ gemini\n╰──────────────┈⊷",
-            "4": "╭━━〔 Tools Menu 〕━━┈⊷\n┃◈ calculator\n┃◈ tempmail\n┃◈ checkmail\n┃◈ tts\n╰──────────────┈⊷",
-            "5": "╭━━〔 Group Menu 〕━━┈⊷\n┃◈ linkgroup\n┃◈ setppgc\n┃◈ setname\n┃◈ setdesc\n┃◈ group\n┃◈ welcome\n┃◈ add\n┃◈ kick\n┃◈ antilink\n┃◈ promote\n╰──────────────┈⊷",
-            "6": "╭━━〔 Search Menu 〕━━┈⊷\n┃◈ play\n┃◈ yts\n┃◈ imdb\n┃◈ google\n┃◈ pinterest\n┃◈ wallpaper\n┃◈ wikimedia\n┃◈ lyrics\n╰──────────────┈⊷",
-            "7": "╭━━〔 Main Menu 〕━━┈⊷\n┃◈ ping\n┃◈ alive\n┃◈ owner\n┃◈ menu\n╰──────────────┈⊷",
-            "8": "╭━━〔 Owner Menu 〕━━┈⊷\n┃◈ join\n┃◈ leave\n┃◈ block\n┃◈ unblock\n┃◈ setppbot\n┃◈ anticall\n┃◈ alwaysonline\n┃◈ autoread\n╰──────────────┈⊷",
-            "9": "╭━━〔 Stalk Menu 〕━━┈⊷\n┃◈ truecaller\n┃◈ instastalk\n┃◈ githubstalk\n╰──────────────┈⊷"
+            "1": "
+  ╭━━〔 Download Menu 〕━━┈⊷
+\n┃◈ apk
+\n┃◈ facebook
+\n┃◈ mediafire
+\n┃◈ pinterestdl
+\n┃◈ insta
+\n┃◈ ytmp3
+\n┃◈ ytmp4
+\n┃◈ play
+\n┃◈ song
+\n┃◈ video
+\n╰──────────────┈⊷",
+            "2": "
+  ╭━━〔 Converter Menu 〕━━┈⊷
+\n┃◈ attp
+\n┃◈ ebinary
+\n┃◈ dbinary
+\n┃◈ emojimix
+\n┃◈ mp3
+\n┃◈ url
+\n╰──────────────┈⊷",
+            "3": "
+  ╭━━〔 AI Menu 〕━━┈⊷
+\n┃◈ ai
+\n┃◈ sheng on/off
+\n┃◈ report
+\n┃◈ chatbot on/off
+\n┃◈ dalle
+\n┃◈ gemini
+\n╰──────────────┈⊷",
+            "4": "
+  ╭━━〔 Tools Menu 〕━━┈⊷
+\n┃◈ calculator
+\n┃◈ tempmail
+\n┃◈ checkmail
+\n┃◈ tts
+\n╰──────────────┈⊷",
+            "5": "
+  ╭━━〔 Group Menu 〕━━┈⊷
+\n┃◈ linkgroup
+\n┃◈ setppgc
+\n┃◈ setname
+\n┃◈ setdesc
+\n┃◈ antibot on/off
+\n┃◈ welcome
+\n┃◈ add
+\n┃◈ kick
+\n┃◈ antilink on/off
+\n┃◈ promote
+\n┃◈ open
+\n┃◈ close
+\n┃◈ vcf
+\n╰──────────────┈⊷",
+            "6": "
+╭━━〔 Search Menu 〕━━┈⊷
+\n┃◈ play
+\n┃◈ yts
+\n┃◈ imdb
+\n┃◈ google
+\n┃◈ pinterest
+\n┃◈ wallpaper
+\n┃◈ wikimedia
+\n┃◈ lyrics
+\n╰──────────────┈⊷",
+            "7": "
+  ╭━━〔 Main Menu 〕━━┈⊷
+\n┃◈ ping
+\n┃◈ alive
+\n┃◈ owner
+\n┃◈ menu
+\n╰──────────────┈⊷",
+            "8": "
+  ╭━━〔 Owner Menu 〕━━┈⊷
+\n┃◈ join
+\n┃◈ leave
+\n┃◈ block
+\n┃◈ unblock
+\n┃◈ setppbot
+\n┃◈ anticall
+\n┃◈ alwaysonline
+\n┃◈ autoread
+\n┃◈ pp <set profile>
+\n┃◈ update 
+\n╰──────────────┈⊷",
+            "9": "
+  ╭━━〔 Stalk Menu 〕━━┈⊷
+\n┃◈ truecaller
+\n┃◈ instastalk
+\n┃◈ githubstalk
+\n╰──────────────┈⊷"
         };
 
         const menuResponse = menus[receivedText] || "*Invalid Reply! Please Reply With A Number Between 1 to 9*";
