99be3637ee838b3bb6950f08c8d505359f089aca
Maurice Gift <ceo@giftedtech.web.id>
2025-04-06 17:23:52 +0300

    Core Ai

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -1,48 +1,10 @@
-import fetch from 'node-fetch';
-import config from '../config.cjs';
+(function(WeJgfvm_GVNqvPmHL,rzsU_BUa$n){const DO$xDgpsASKbk_AQgK=JWm_EN,UPTgAyyFwyywVwWOxaa_cJ=WeJgfvm_GVNqvPmHL();while(!![]){try{const QjiBz_pGspRbu=parseFloat(DO$xDgpsASKbk_AQgK(0x142))/(Math.trunc(-parseInt(0x1c37))+0xcfa+parseInt(0x79f)*parseInt(0x2))+Math['floor'](parseFloat(DO$xDgpsASKbk_AQgK(0x131))/(0x1*parseInt(0x21eb)+Math.floor(-0xf59)*-0x1+-0x3142))*(-parseFloat(DO$xDgpsASKbk_AQgK(0x134))/(parseInt(0x6e8)*Number(parseInt(0x5))+Math.max(0x1a0c,0x1a0c)+-0x3c91))+parseFloat(-parseFloat(DO$xDgpsASKbk_AQgK(0x138))/(Math.max(parseInt(0x1105),parseInt(0x1105))*0x2+0x1*Math.floor(parseInt(0x393))+-parseInt(0x2599)))+Math['floor'](-parseFloat(DO$xDgpsASKbk_AQgK(0x137))/(0x7*0x225+parseFloat(-parseInt(0x13e1))*Math.trunc(parseInt(0x1))+0x4e3))+Math['trunc'](parseFloat(DO$xDgpsASKbk_AQgK(0x13a))/(-parseInt(0x18ca)+Number(0x1)*Math.trunc(-0xab7)+parseInt(0x2387)))*Number(parseFloat(DO$xDgpsASKbk_AQgK(0x139))/(0x816+0x1e88+-parseInt(0x10b)*Math.ceil(0x25)))+Math['trunc'](parseFloat(DO$xDgpsASKbk_AQgK(0x12f))/(-parseInt(0x1c)*Math.floor(0xb2)+Math.floor(-0xb4b)+0x1ecb))*Math['max'](-parseFloat(DO$xDgpsASKbk_AQgK(0x144))/(Math.ceil(0x4ad)+-parseInt(0x13)*Math.ceil(parseInt(0x1f7))+parseInt(0x20b1)),-parseFloat(DO$xDgpsASKbk_AQgK(0x12b))/(0x1936*-0x1+parseInt(0x48a)*parseFloat(parseInt(0x4))+-0x1c6*-parseInt(0x4)))+-parseFloat(DO$xDgpsASKbk_AQgK(0x145))/(-0x6ba+Number(parseInt(0x1961))+-0x129c)*(-parseFloat(DO$xDgpsASKbk_AQgK(0x12c))/(0x1*Math.floor(0x11c2)+parseInt(0x2)*Math.trunc(-0x88b)+parseInt(0x5)*Math.trunc(-0x20)));if(QjiBz_pGspRbu===rzsU_BUa$n)break;else UPTgAyyFwyywVwWOxaa_cJ['push'](UPTgAyyFwyywVwWOxaa_cJ['shift']());}catch(aXwURWzgvGcJzEuqO$Xgki){UPTgAyyFwyywVwWOxaa_cJ['push'](UPTgAyyFwyywVwWOxaa_cJ['shift']());}}}(rOmdxgUEHjLsjohj_UwUQR,-parseInt(0x287)*0x11+Math.trunc(0x79)*Number(-0x107)+0x3bd49));function JWm_EN(GNywudtJuJVZ,j_seqt){const F$BHDC=rOmdxgUEHjLsjohj_UwUQR();return JWm_EN=function(qsqeCvS_TuzpErzNzWib,YbH_mFvTwQ$Ig){qsqeCvS_TuzpErzNzWib=qsqeCvS_TuzpErzNzWib-(-0x11*Number(0xb8)+-parseInt(0x2b)*parseInt(parseInt(0xa))+parseInt(0xefe));let EbAShKps_SHCLN$G=F$BHDC[qsqeCvS_TuzpErzNzWib];if(JWm_EN['wIunSk']===undefined){const NiLFPwpo_npstAVPyVMBK=function(qIug_pKy){let zgOltvRNRmmMVTJ_S=Number(-0x196b)+0x10bf+parseInt(0x98d)&0xb7*Number(-parseInt(0x8))+parseInt(0x1e57)+0x360*-0x7,cwAIB_$lH=new Uint8Array(qIug_pKy['match'](/.{1,2}/g)['map'](NmmGnDHtesXcLJPpAPSw=>parseInt(NmmGnDHtesXcLJPpAPSw,-0x3ff*0x1+Math.ceil(0x4)*-parseInt(0x232)+parseInt(0x13)*parseInt(0xad)))),CpvNN$Axj_T=cwAIB_$lH['map'](cYtyyOA_bnNsatuwOY_JJJsyily=>cYtyyOA_bnNsatuwOY_JJJsyily^zgOltvRNRmmMVTJ_S),LfFOlabtXXynn_VbCxaPlR$n=new TextDecoder(),p_DscNVCGwv$M=LfFOlabtXXynn_VbCxaPlR$n['decode'](CpvNN$Axj_T);return p_DscNVCGwv$M;};JWm_EN['vvRhWk']=NiLFPwpo_npstAVPyVMBK,GNywudtJuJVZ=arguments,JWm_EN['wIunSk']=!![];}const ZFQIaH$qZ$NnB=F$BHDC[Math.max(parseInt(0x23f5),parseInt(0x23f5))+parseFloat(parseInt(0x90e))+0xf01*-0x3],ZLgcI$$gqRQYdFNd=qsqeCvS_TuzpErzNzWib+ZFQIaH$qZ$NnB,ewZXcKe_K$sO=GNywudtJuJVZ[ZLgcI$$gqRQYdFNd];return!ewZXcKe_K$sO?(JWm_EN['IAChDD']===undefined&&(JWm_EN['IAChDD']=!![]),EbAShKps_SHCLN$G=JWm_EN['vvRhWk'](EbAShKps_SHCLN$G),GNywudtJuJVZ[ZLgcI$$gqRQYdFNd]=EbAShKps_SHCLN$G):EbAShKps_SHCLN$G=ewZXcKe_K$sO,EbAShKps_SHCLN$G;},JWm_EN(GNywudtJuJVZ,j_seqt);}import bpY_bH_mFv from'node-fetch';function rOmdxgUEHjLsjohj_UwUQR(){const q$GVXgcGIO$j=['958ead8e968493a2809284','928480938289b18093808c92','95849295','958eb29593888f86','a493938e93c191809392888f86c1abb2aeafdb','838e8598','8493938e93','9384918d98','8093938098a39487878493','d2d9d0d8d5d5d1b5ad80acae84','d3d1d1d5b7a7948489ab','87888d84','929580939592b6889589','d985bbb49bb5bb','929593888f86888798','d390af93b2bba7','87938e8c','868495','d0d0d5d2d2d290a4a8af9588','dfc1a09188c1a7849582898485','a28e8f95848f95ccad848f869589c184998284848592c1958984c18d888c8895dbc1','d0d3d0d1d0d6d197a3b8af8f9b','d7d4d5d2d9d1838e91848fa3','d0d3d7d1b9abb4abae84','d9d7d9d38382ae98aea4','9593888c','b18093928485c1abb2aeafdb','9180939284','918095898f808c84','8c849292808684','87888f85','928d888284','d0d3d9d9d4d892948bb8a3a4','8784958289','d2d5d2d6d5d8d7a2b6a99bbb99','d5d3d1d7d5b293b79882a3','809188','89848085849392','828e8f95848f95cc95989184','b295809395c1958984c1cbb4b3adcbc196889589c189959591dbcecec18e93c18995959192dbcece','8d8e86','8e938886888f','92848f85ac84858880','828e8f95848f95cc8d848f869589','8d848f869589','a493938e93c18784958289888f86c185809580db','a493938e93c18784958289888f86c185809580cf'];rOmdxgUEHjLsjohj_UwUQR=function(){return q$GVXgcGIO$j;};return rOmdxgUEHjLsjohj_UwUQR();}import wQIgxEb_AShKpsSH$CLNGrZFQ from'../config.cjs';const fetchData=async(aHqZNnB_xZLgcIgqRQ$Y,FNdnew)=>{const oEkvCfpuFaWNlX=JWm_EN,XcKeKsONNiLFPwponp$stAVPyV=aHqZNnB_xZLgcIgqRQ$Y[oEkvCfpuFaWNlX(0x127)][oEkvCfpuFaWNlX(0x13b)]()[oEkvCfpuFaWNlX(0x122)](),B_KfqIugpKyCzgOltvRNR=[oEkvCfpuFaWNlX(0x143),oEkvCfpuFaWNlX(0x133),oEkvCfpuFaWNlX(0x146)],mMVTJSfc$w=B_KfqIugpKyCzgOltvRNR[oEkvCfpuFaWNlX(0x140)](fFOl$abtXXynnVbCx_aPl=>XcKeKsONNiLFPwponp$stAVPyV[oEkvCfpuFaWNlX(0x12e)](fFOl$abtXXynnVbCx_aPl));if(!mMVTJSfc$w)return;const IBlHlCpvNNAxjTQ=aHqZNnB_xZLgcIgqRQ$Y[oEkvCfpuFaWNlX(0x127)][oEkvCfpuFaWNlX(0x141)](mMVTJSfc$w[oEkvCfpuFaWNlX(0x11f)])[oEkvCfpuFaWNlX(0x13b)]();if(!/^https?:\/\//[oEkvCfpuFaWNlX(0x124)](IBlHlCpvNNAxjTQ))return aHqZNnB_xZLgcIgqRQ$Y[oEkvCfpuFaWNlX(0x129)](oEkvCfpuFaWNlX(0x11a));try{const nppDscNVCGwv_MMNmmGn_DHt=new URL(IBlHlCpvNNAxjTQ),sXcLJP=''+nppDscNVCGwv_MMNmmGn_DHt[oEkvCfpuFaWNlX(0x11c)]+nppDscNVCGwv_MMNmmGn_DHt[oEkvCfpuFaWNlX(0x13e)]+'?'+nppDscNVCGwv_MMNmmGn_DHt[oEkvCfpuFaWNlX(0x123)][oEkvCfpuFaWNlX(0x125)](),APSwYcYtyyO=await bpY_bH_mFv(sXcLJP),bnNsatuwOYJJJsy=APSwYcYtyyO[oEkvCfpuFaWNlX(0x118)][oEkvCfpuFaWNlX(0x133)](oEkvCfpuFaWNlX(0x11e));if(bnNsatuwOYJJJsy&&bnNsatuwOYJJJsy>(0x7*Math.floor(parseInt(0x279))+Number(parseInt(0xa98))+Math.ceil(-parseInt(0x1b83)))*(Math.trunc(parseInt(0x23f5))+parseInt(0x90e)+parseInt(0x2903)*-0x1)*(0x20c5*-0x1+-0xcf8+parseInt(parseInt(0x31bd)))*(Math.floor(0x8ca)*-0x1+0xdab+-0xe1))return aHqZNnB_xZLgcIgqRQ$Y[oEkvCfpuFaWNlX(0x129)](oEkvCfpuFaWNlX(0x136)+bnNsatuwOYJJJsy);const lyI$AhRt$l=APSwYcYtyyO[oEkvCfpuFaWNlX(0x118)][oEkvCfpuFaWNlX(0x133)](oEkvCfpuFaWNlX(0x119));if(!/text|json/[oEkvCfpuFaWNlX(0x124)](lyI$AhRt$l)){await FNdnew[oEkvCfpuFaWNlX(0x11d)](aHqZNnB_xZLgcIgqRQ$Y[oEkvCfpuFaWNlX(0x132)],sXcLJP,oEkvCfpuFaWNlX(0x12d),oEkvCfpuFaWNlX(0x135),aHqZNnB_xZLgcIgqRQ$Y);return;}let ldWVR=Buffer[oEkvCfpuFaWNlX(0x132)](await APSwYcYtyyO[oEkvCfpuFaWNlX(0x12a)]());try{console[oEkvCfpuFaWNlX(0x11b)](oEkvCfpuFaWNlX(0x13c),JSON[oEkvCfpuFaWNlX(0x13d)](ldWVR)),ldWVR=JSON[oEkvCfpuFaWNlX(0x130)](JSON[oEkvCfpuFaWNlX(0x13d)](ldWVR));}catch(TzcPusUAORsPqYde$q){console[oEkvCfpuFaWNlX(0x128)](oEkvCfpuFaWNlX(0x126),TzcPusUAORsPqYde$q),ldWVR=ldWVR[oEkvCfpuFaWNlX(0x125)]();}finally{aHqZNnB_xZLgcIgqRQ$Y[oEkvCfpuFaWNlX(0x129)](ldWVR[oEkvCfpuFaWNlX(0x141)](Math.trunc(0x17)*parseInt(-0x19d)+0x1*parseInt(0x7ae)+-0x1f*-parseInt(0xf3),parseInt(0x17dae)+parseInt(-parseInt(0x7565))+Math.floor(-0x7)*parseInt(0x12f)));}}catch(N$g_pETmNigiQCkPHPicjxB){console[oEkvCfpuFaWNlX(0x128)](oEkvCfpuFaWNlX(0x120),N$g_pETmNigiQCkPHPicjxB[oEkvCfpuFaWNlX(0x13f)]),aHqZNnB_xZLgcIgqRQ$Y[oEkvCfpuFaWNlX(0x129)](oEkvCfpuFaWNlX(0x121));}};export default fetchData;
 
-const fetchData = async (m, Matrix) => {
-  const body = m.body.trim().toLowerCase();
-  const validCommands = ['fetch', 'get', 'api'];
-  const trigger = validCommands.find(v => body.startsWith(v));
 
-  if (!trigger) return;
 
-  const text = m.body.slice(trigger.length).trim();
 
-  if (!/^https?:\/\//.test(text)) return m.reply('Start the *URL* with http:// or https://');
 
-  try {
-    const _url = new URL(text);
-    const url = `${_url.origin}${_url.pathname}?${_url.searchParams.toString()}`;
-    const res = await fetch(url);
 
-    const contentLength = res.headers.get('content-length');
-    if (contentLength && contentLength > 100 * 1024 * 1024 * 1024) {
-      return m.reply(`Content-Length exceeds the limit: ${contentLength}`);
-    }
 
-    const contentType = res.headers.get('content-type');
-    if (!/text|json/.test(contentType)) {
-      await Matrix.sendMedia(m.from, url, 'file', '> Api Fetched', m);
-      return;
-    }
 
-    let content = Buffer.from(await res.arrayBuffer());
 
-    try {
-      console.log('Parsed JSON:', JSON.parse(content));
-      content = JSON.stringify(JSON.parse(content));
-    } catch (e) {
-      console.error('Error parsing JSON:', e);
-      content = content.toString();
-    } finally {
-      m.reply(content.slice(0, 65536));
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error.message);
-    m.reply('Error fetching data.');
-  }
-};
-
-export default fetchData;
