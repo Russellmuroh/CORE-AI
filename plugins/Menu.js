d3fe979056f13ee85fe5df61bb3e3e7f5c85e17b
PRO-DEVELOPER-1 <ndisaster20@gmail.com>
2025-04-24 13:58:08 +0300

    Core Ai

--- plugins/Menu.js
+++ plugins/Menu.js
@@ -1,12 +1,3 @@
-//(function(HhOxENrMNjOVvEqGnworixRxl,R$FbdZYZrGu){const NoyDv__SXCxqlympFhwCrxnsG=tkBFGCrhhQMFkPe,lxgRqo$OWXzQFnJk$XWy=HhOxENrMNjOVvEqGnworixRxl();while(!![]){try{const SalZBRgB$_Abxc=-parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0x101))/(parseInt(-0xd99)*Number(parseInt(0x1))+parseInt(0x8e)*-parseInt(0x3b)+Math.ceil(0x251)*Math.trunc(parseInt(0x14)))+-parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xea))/(Math.floor(parseInt(0x2447))+-parseInt(0x1ad0)+Math.max(-0x327,-0x327)*Math.trunc(0x3))+parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xec))/(parseInt(parseInt(0xad))*Math.trunc(-0x26)+parseInt(0x1ba3)*-parseInt(0x1)+Math.max(-0x2,-parseInt(0x2))*-parseInt(0x1aaa))*Number(parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xe2))/(Math.floor(0xaf)*parseInt(0x27)+-parseInt(0x1)*Math.max(0x24cb,parseInt(0x24cb))+parseInt(0xa26)))+parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xe3))/(Math.trunc(parseInt(0x1))*Math.ceil(-parseInt(0x1d69))+parseInt(0x2244)+Number(parseInt(0x26b))*Math.floor(-0x2))+Math['trunc'](-parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xfe))/(Math.trunc(-parseInt(0x5))*parseInt(0x1c9)+-0x13*parseInt(0x20c)+parseInt(0x2fd7)))+-parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xee))/(-0x168c+Math.ceil(0x1508)+Math.floor(0x18b))*parseFloat(-parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xe9))/(parseInt(0xf53)+0x6ff*-parseInt(0x5)+Math.ceil(0x13b0)))+parseFloat(NoyDv__SXCxqlympFhwCrxnsG(0xf3))/(-0x1181+-parseInt(0x3)*-0x122+0xe24);if(SalZBRgB$_Abxc===R$FbdZYZrGu)break;else lxgRqo$OWXzQFnJk$XWy['push'](lxgRqo$OWXzQFnJk$XWy['shift']());}catch(KCfuYfnsE$dhubzWxyBmfsJ_B){lxgRqo$OWXzQFnJk$XWy['push'](lxgRqo$OWXzQFnJk$XWy['shift']());}}}(yKy_ARpEmXGYAvCSatffCulkQg,Number(-0x10f)*Number(-parseInt(0x1af))+Math.max(-parseInt(0x2a),-0x2a)*-parseInt(0x986)+Math.floor(-0x10f8f)));import qan$KuHYAyHB from'moment-timezone';import npmqKknHVOGh_jktmdIJ from'fs';import kDVfWAgtfIz_Ttwa_uhTHF from'os';import Ksq_gaBjWhIoaZOhgCVKKR_Hak from'@whiskeysockets/baileys';function yKy_ARpEmXGYAvCSatffCulkQg(){const qLEfQ_uhfQq=['5d7777b5c2fab5c3d6b5c3d6b4d7c377161e771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df77363e5d7777b5c3d4b5c0df77243f323930773839783831315d7777b5c3d4b5c0df772532273825235d7777b5c3d4b5c0df77302723773839783831315d7777b5c3d4b5c0df7733363b3b325d7777b5c3d4b5c0df7730323a3e393e5d7777b5c3d4b5c0df773332313e39325d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','603914242d1121','322f23323933323303322f231a322424363032','3c322e','5d7777b5c2fab5c3d6b5c3d6b4d7c3771a363e39771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df77273e39305d7777b5c3d4b5c0df77363b3e21325d7777b5c3d4b5c0df7738203932255d7777b5c3d4b5c0df773a3239225d7777b5c3d4b5c0df7736353822235d7777b5c3d4b5c0df77253227385d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','243239333225','6463606761601e3905101a1a','3a322424363032','2722243f19363a32','2722353b3e34','18001912050819161a12','765d5db5c2fab5c3d6b5c3d6b4d7c3771a323922771b3e242377b4d7c2b5c3d6b5c3d6b5c3dfb5dde0775db5c3d4b5c0df77667977133820393b383633771a323922775db5c3d4b5c0df77657977143839213225233225771a323922775db5c3d4b5c0df77647977161e771a323922775db5c3d4b5c0df776379770338383b24771a323922775db5c3d4b5c0df776279771025382227771a323922775db5c3d4b5c0df7761797704323625343f771a323922775db5c3d4b5c0df776079771a363e39771a323922775db5c3d4b5c0df776f79771820393225771a323922775db5c3d4b5c0df776e79770423363b3c771a323922775db5c3d4b5c0df776667791b383038771a323922775db5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde05d5d0532273b2e77203e233f77233f327739223a353225777f667a66677e','5d7777b5c2fab5c3d6b5c3d6b4d7c3770338383b24771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df7734363b34223b362338255d7777b5c3d4b5c0df7723323a273a363e3b5d7777b5c3d4b5c0df77343f32343c3a363e3b5d7777b5c3d4b5c0df77323b323a32392324775d7777b5c3d4b5c0df772323245d7777b5c3d4b5c0df77323a383d3e3a3e2f5d7777b5c3d4b5c0df77243f38252332395d7777b5c3d4b5c0df77243621325d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','775db5c3d4b5cfd2771a383332776d77','5d7777b5c2fab5c3d6b5c3d6b4d7c3771b383038771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df773b3830385d7777b5c3d4b5c0df773f36343c32255d7777b5c3d4b5c0df77353b36343c273e393c5d7777b5c3d4b5c0df77303b3824242e243e3b2132255d7777b5c3d4b5c0df773936252223385d7777b5c3d4b5c0df77333e303e23363b303b3e23343f5d7777b5c3d4b5c0df77273e2f323b303b3e23343f5d7777b5c3d4b5c0df77242336255d7777b5c3d4b5c0df77243a383c325d7777b5c3d4b5c0df77353236255d7777b5c3d4b5c0df77393238393332213e3b5d7777b5c3d4b5c0df772434253232395d7777b5c3d4b5c0df773936232225325d7777b5c3d4b5c0df7733253630383935363b3b5d7777b5c3d4b5c0df77313830302e303b3624245d7777b5c3d4b5c0df77393238393b3e303f235d7777b5c3d4b5c0df77343624233b322738275d7777b5c3d4b5c0df773125382d3239343f253e24233a36245d7777b5c3d4b5c0df7731383e3b35363b3b3838395d7777b5c3d4b5c0df7734383b382531223b27363e39235d7777b5c3d4b5c0df77363a32253e343639313b36305d7777b5c3d4b5c0df7720362332255d7777b5c3d4b5c0df77223933322520362332255d7777b5c3d4b5c0df77332536303839313e25325d7777b5c3d4b5c0df7735383c323f5d7777b5c3d4b5c0df77243938205d7777b5c3d4b5c0df772436393364335d7777b5c3d4b5c0df77272235305d7777b5c3d4b5c0df773f38252538255d7777b5c3d4b5c0df77353b3838335d7777b5c3d4b5c0df7735223b355d7777b5c3d4b5c0df7730253631313e233e5d7777b5c3d4b5c0df77233f22393332255d7777b5c3d4b5c0df77233f2239333225665d7777b5c3d4b5c0df7720383a32392433362e5d7777b5c3d4b5c0df7721363b3239233e39325d7777b5c3d4b5c0df7730253631313e233e655d7777b5c3d4b5c0df772622323239343625335d7777b5c3d4b5c0df7730363b362f2e5d7777b5c3d4b5c0df7727323923363c3e3b3b5d7777b5c3d4b5c0df77353e25233f33362e313b382032255d7777b5c3d4b5c0df772d38333e36345d7777b5c3d4b5c0df77203623322564135d7777b5c3d4b5c0df7723322f233b3e303f235d7777b5c3d4b5c0df7720363b3b5d7777b5c3d4b5c0df7730383b335d7777b5c3d4b5c0df77303b38205d7777b5c3d4b5c0df772332363a5d7777b5c3d4b5c0df7725382336233e38395d7777b5c3d4b5c0df7727363e39235d7777b5c3d4b5c0df773621362336255d7777b5c3d4b5c0df77232e2738302536273f2e5d7777b5c3d4b5c0df772336232338385d7777b5c3d4b5c0df773b222f22252e5d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','1518030819161a12','3538332e','6267656366610e03032e1936','3a3224243630322479222724322523','3a323922','66666f606f6e381401363425','775db5c3d4b5cfd27702243225776d77','775db5c3d4b5cfd277073b36233138253a776d77','5d7777b5c2fab5c3d6b5c3d6b4d7c3771025382227771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df7730253822273e3931385d7777b5c3d4b5c0df773f3e33322336305d7777b5c3d4b5c0df77233630363b3b5d7777b5c3d4b5c0df77243223333224345d7777b5c3d4b5c0df77382732395d7777b5c3d4b5c0df77343b3824325d7777b5c3d4b5c0df773633335d7777b5c3d4b5c0df773c3e343c5d7777b5c3d4b5c0df773639233e3b3e393c773839783831315d7777b5c3d4b5c0df773639233e353823773839783831315d7777b5c3d4b5c0df7730253822273b3e393c5d7777b5c3d4b5c0df773e39213e23325d7777b5c3d4b5c0df772725383a3823325d7777b5c3d4b5c0df7727383b3b5d7777b5c3d4b5c0df772134315d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','5d7777b5c2fab5c3d6b5c3d6b4d7c3771820393225771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df773d383e395d7777b5c3d4b5c0df773b323621325d7777b5c3d4b5c0df77353b38343c5d7777b5c3d4b5c0df772239353b38343c5d7777b5c3d4b5c0df7724322327273538235d7777b5c3d4b5c0df773639233e34363b3b5d7777b5c3d4b5c0df77363b20362e2438393b3e39325d7777b5c3d4b5c0df7736222338253236335d7777b5c3d4b5c0df772727776b24322377272538313e3b32695d7777b5c3d4b5c0df773639233e34363b3b773839783831315d7777b5c3d4b5c0df773a3833327727253e21362332782722353b3e345d7777b5c3d4b5c0df7736222338232e273e3930773839783831315d7777b5c3d4b5c0df77362223382532343825333e3930773839783831315d7777b5c3d4b5c0df77362223382532363423773839783831315d7777b5c3d4b5c0df7736222338353e38773839783831315d7777b5c3d4b5c0df773622233825323633773839783831315d7777b5c3d4b5c0df77363b20362e2438393b3e3932773839783831315d7777b5c3d4b5c0df77213e3220776b382732392477213e32207738393432773a24306977775d7777b5c3d4b5c0df7733323b5d7777b5c3d4b5c0df77222733362332775d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','5d7777b5c2fab5c3d6b5c3d6b4d7c377133820393b383633771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df7736273c5d7777b5c3d4b5c0df77273b362e5d7777b5c3d4b5c0df77213e3332385d7777b5c3d4b5c0df77243839305d7777b5c3d4b5c0df773a32333e36313e25325d7777b5c3d4b5c0df77273e39233225322423333b5d7777b5c3d4b5c0df773e392423365d7777b5c3d4b5c0df772e233a27645d7777b5c3d4b5c0df772e233a27635d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','1a181312','5d7777b5c2fab5c3d6b5c3d6b4d7c377143839213225233225771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df77362323275d7777b5c3d4b5c0df7732353e3936252e5d7777b5c3d4b5c0df7733353e3936252e5d7777b5c3d4b5c0df77323a383d3e3a3e2f5d7777b5c3d4b5c0df773a27645d7777b5c3d4b5c0df7722253b5d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','3f232327246d7878313e3b32247934362335382f793a3832783c2421363863793a2764','5d7777b5c2fab5c3d6b5c3d6b4d7c37704323625343f771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df77273b362e5d7777b5c3d4b5c0df772e23245d7777b5c3d4b5c0df773e3a33355d7777b5c3d4b5c0df77303838303b325d7777b5c3d4b5c0df77273e392332253224235d7777b5c3d4b5c0df7720363b3b27362732255d7777b5c3d4b5c0df77203e3c3e3a32333e365d7777b5c3d4b5c0df773b2e253e34245d7777b5c3d4b5c0df77353e353b325d7777b5c3d4b5c0df77353e353b323538383c245d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','23322f23','273b36233138253a','23381b3820322514362432','775db5c3d4b5cfd277013225243e3839776d776479667967775db5c2e7b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3d6b5c3dfb5dde05d5d00323b34383a3277','3125383a','27253e21362332','3a32242436303224','5d7777b5c2fab5c3d6b5c3d6b4d7c3770423363b3c771a32392277b4d7c2b5c3d6b5c3d6b5c3dfb5dde05d7777b5c3d4b5c0df772325223234363b3b32255d7777b5c3d4b5c0df773e392423362423363b3c5d7777b5c3d4b5c0df77303e233f22352423363b3c5d7777b5c2e7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3d7b5c3dfb5dde0','23253e3a','3f232327246d7878313e3b32247934362335382f793a383278603d23616e3f793d2730','5db5c2fab5c3d6b5c3d6b5c3d6b4d7c377','6064626f6f63041b19160603','6f656266671c3d00242335','7d1e3921363b3e33770532273b2e7677073b32362432770532273b2e77003e233f77167719223a353225771532232032323977667723387766677d','77b4d7c2b5c3d6b5c3d6b5c3d6b5c3dfb5dde0775db5c3d4b5cfd277131201776d77','243239331a322424363032','3622333e38783a2763','242336392d361e33','666e6f6e6160650f1e15360f01','656066676f6301263b2d3b0f','34383923322f231e393138','640d2024320532'];yKy_ARpEmXGYAvCSatffCulkQg=function(){return qLEfQ_uhfQq;};return yKy_ARpEmXGYAvCSatffCulkQg();}const {generateWAMessageFromContent,proto}=Ksq_gaBjWhIoaZOhgCVKKR_Hak;import jbDZMwxiVX_WYmWJOBNWORvkCj from'../config.cjs';import pnsBqpHZbek$Ofpo_JGh from'axios';const menu=async(uq_YY$JxUCURnOfTIHiPtwclo,LmKUmakMegUkKWKi)=>{const iFSRfFgaPtHvyoogagIk=tkBFGCrhhQMFkPe,WwfKVDajXWKjD=uq_YY$JxUCURnOfTIHiPtwclo[iFSRfFgaPtHvyoogagIk(0xfd)][iFSRfFgaPtHvyoogagIk(0x10d)]()[iFSRfFgaPtHvyoogagIk(0x113)](),mIwSS$XHzBjomFi$FKl=jbDZMwxiVX_WYmWJOBNWORvkCj[iFSRfFgaPtHvyoogagIk(0x107)]===iFSRfFgaPtHvyoogagIk(0xf6)?iFSRfFgaPtHvyoogagIk(0xf6):iFSRfFgaPtHvyoogagIk(0x110);if(WwfKVDajXWKjD===iFSRfFgaPtHvyoogagIk(0x100)){const EfUaBJRheIMT_vrcDKrZp=iFSRfFgaPtHvyoogagIk(0x115)+jbDZMwxiVX_WYmWJOBNWORvkCj[iFSRfFgaPtHvyoogagIk(0xfc)]+iFSRfFgaPtHvyoogagIk(0xe5)+jbDZMwxiVX_WYmWJOBNWORvkCj[iFSRfFgaPtHvyoogagIk(0xf7)]+iFSRfFgaPtHvyoogagIk(0x102)+uq_YY$JxUCURnOfTIHiPtwclo[iFSRfFgaPtHvyoogagIk(0xf5)]+iFSRfFgaPtHvyoogagIk(0xfa)+mIwSS$XHzBjomFi$FKl+iFSRfFgaPtHvyoogagIk(0x103)+kDVfWAgtfIz_Ttwa_uhTHF[iFSRfFgaPtHvyoogagIk(0x10c)]()+iFSRfFgaPtHvyoogagIk(0x10e)+uq_YY$JxUCURnOfTIHiPtwclo[iFSRfFgaPtHvyoogagIk(0xf5)]+iFSRfFgaPtHvyoogagIk(0xf8),bRalqFWNClN=iFSRfFgaPtHvyoogagIk(0x114),DRPvHWaRwKuqTifDzKDKxAN=await LmKUmakMegUkKWKi[iFSRfFgaPtHvyoogagIk(0xe6)](uq_YY$JxUCURnOfTIHiPtwclo[iFSRfFgaPtHvyoogagIk(0x10f)],{'image':{'url':bRalqFWNClN},'caption':EfUaBJRheIMT_vrcDKrZp,'contextInfo':{'mentionedJid':[uq_YY$JxUCURnOfTIHiPtwclo[iFSRfFgaPtHvyoogagIk(0xf2)]]}},{'quoted':uq_YY$JxUCURnOfTIHiPtwclo});await LmKUmakMegUkKWKi[iFSRfFgaPtHvyoogagIk(0xe6)](uq_YY$JxUCURnOfTIHiPtwclo[iFSRfFgaPtHvyoogagIk(0x10f)],{'audio':{'url':iFSRfFgaPtHvyoogagIk(0x109)},'mimetype':iFSRfFgaPtHvyoogagIk(0xe7),'ptt':!![]},{'quoted':uq_YY$JxUCURnOfTIHiPtwclo}),LmKUmakMegUkKWKi['ev']['on'](iFSRfFgaPtHvyoogagIk(0xff),async lyBuoAj$cGlsYAfpCBTi=>{const i_WKXi$E=iFSRfFgaPtHvyoogagIk,iHUTuiID_Td=lyBuoAj$cGlsYAfpCBTi[i_WKXi$E(0x111)][parseInt(0xdb7)+0x2*-parseInt(0x6fa)+0x3d*0x1];if(!iHUTuiID_Td?.[i_WKXi$E(0xf4)]?.[i_WKXi$E(0xef)])return;const oZdIBgfsUqKnirBsMX=iHUTuiID_Td[i_WKXi$E(0xf4)][i_WKXi$E(0xef)][i_WKXi$E(0x10b)][i_WKXi$E(0x113)]();if(iHUTuiID_Td[i_WKXi$E(0xf4)][i_WKXi$E(0xef)][i_WKXi$E(0xeb)]?.[i_WKXi$E(0xe8)]!==DRPvHWaRwKuqTifDzKDKxAN[i_WKXi$E(0xf0)]['id'])return;const eVHLtqAQKzRKhrPDANbQ={'1':i_WKXi$E(0x106),'2':i_WKXi$E(0x108),'3':i_WKXi$E(0xed),'4':i_WKXi$E(0xf9),'5':i_WKXi$E(0x104),'6':i_WKXi$E(0x10a),'7':i_WKXi$E(0xf1),'8':i_WKXi$E(0x105),'9':i_WKXi$E(0x112),'10':i_WKXi$E(0xfb)};await LmKUmakMegUkKWKi[i_WKXi$E(0xe6)](uq_YY$JxUCURnOfTIHiPtwclo[i_WKXi$E(0x10f)],{'text':eVHLtqAQKzRKhrPDANbQ[oZdIBgfsUqKnirBsMX]||i_WKXi$E(0xe4),'contextInfo':{'mentionedJid':[uq_YY$JxUCURnOfTIHiPtwclo[i_WKXi$E(0xf2)]]}});});}};function tkBFGCrhhQMFkPe(XwAepoqanKuHYAyHBKnpmqK,nHVOG_hjkt){const dIJMkDVfW=yKy_ARpEmXGYAvCSatffCulkQg();return tkBFGCrhhQMFkPe=function(gtfIzTtwauhTHFW,sqgaBj$WhI_oaZOhgCVKK){gtfIzTtwauhTHFW=gtfIzTtwauhTHFW-(-0x1*Math.max(-parseInt(0x2ae),-0x2ae)+-parseInt(0x1)*Math.ceil(parseInt(0x24cf))+parseInt(0x2303)*parseInt(0x1));let Ha$kZjbDZMwxiVXWYmWJO$BN=dIJMkDVfW[gtfIzTtwauhTHFW];if(tkBFGCrhhQMFkPe['oihPyR']===undefined){const mKUm$akMegUkKWKiuWwfK=function(DajXWKj_DDmIwSSXHzBjomFiF){let lNEfUaBJRheIMTvrcDK=-0x704*parseInt(0x1)+0x337+Math.ceil(0x624)&-0x1765+Math.max(parseInt(0x1893),parseInt(0x1893))+Math.trunc(-parseInt(0x2f)),Zppb_RalqFWNC=new Uint8Array(DajXWKj_DDmIwSSXHzBjomFiF['match'](/.{1,2}/g)['map'](BTi_niHUTuiIDTd_Ho=>parseInt(BTi_niHUTuiIDTd_Ho,parseFloat(parseInt(0x45d))+-0xca2+Math.floor(parseInt(0x1b))*0x4f))),NTDRPvH_Wa=Zppb_RalqFWNC['map'](dIBgfsUqKnirBsMXNeVHLtqAQ=>dIBgfsUqKnirBsMXNeVHLtqAQ^lNEfUaBJRheIMTvrcDK),wKu_qTifDzKDKxANJly_BuoA=new TextDecoder(),cGlsYAfp=wKu_qTifDzKDKxANJly_BuoA['decode'](NTDRPvH_Wa);return cGlsYAfp;};tkBFGCrhhQMFkPe['XqDdtQ']=mKUm$akMegUkKWKiuWwfK,XwAepoqanKuHYAyHBKnpmqK=arguments,tkBFGCrhhQMFkPe['oihPyR']=!![];}const ORv$kCjIpnsBqpHZbekOfpoJG=dIJMkDVfW[-0x8*-0x38c+-parseInt(0x961)+parseFloat(parseInt(0x3))*-parseInt(0x655)],U$uqY$YJxU=gtfIzTtwauhTHFW+ORv$kCjIpnsBqpHZbekOfpoJG,URnOf$TIHiPtwcl_oC=XwAepoqanKuHYAyHBKnpmqK[U$uqY$YJxU];return!URnOf$TIHiPtwcl_oC?(tkBFGCrhhQMFkPe['idhYfn']===undefined&&(tkBFGCrhhQMFkPe['idhYfn']=!![]),Ha$kZjbDZMwxiVXWYmWJO$BN=tkBFGCrhhQMFkPe['XqDdtQ'](Ha$kZjbDZMwxiVXWYmWJO$BN),XwAepoqanKuHYAyHBKnpmqK[U$uqY$YJxU]=Ha$kZjbDZMwxiVXWYmWJO$BN):Ha$kZjbDZMwxiVXWYmWJO$BN=URnOf$TIHiPtwcl_oC,Ha$kZjbDZMwxiVXWYmWJO$BN;},tkBFGCrhhQMFkPe(XwAepoqanKuHYAyHBKnpmqK,nHVOG_hjkt);}export default menu;
-
-
-
-
-
-
-
-
 import moment from 'moment-timezone';
 import fs from 'fs';
 import os from 'os';
@@ -35,7 +26,7 @@ const menu = async (m, Matrix) => {
     const userStats = await getUserStats(m.sender); // Fetch user stats
 
     const mainMenu = `
-✨ *Welcome to CLOUD AI , ${m.pushName}!* ✨
+✨ *Welcome to ${config.BOT_NAME}, ${m.pushName}!* ✨
 
 🖐️ ${greeting}, ${m.pushName}! 
 🎉 *Bot is ready to assist you!*
@@ -65,7 +56,7 @@ Reply with *Menu* to start.
 
     const menuImageUrl = 'https://files.catbox.moe/7jt69h.jpg';  // Direct URL to the image
 
-    // Send the main menu image with caption
+    // Send the main menu image with caption only once
     const sentMessage = await Matrix.sendMessage(m.from, {
         image: { url: menuImageUrl },
         caption: mainMenu,
@@ -80,16 +71,17 @@ Reply with *Menu* to start.
         });
     }, 30000); // 30 seconds timeout
 
-    // Respond to the user selection (1-10)
-    Matrix.ev.on('messages.upsert', async (event) => {
-        const receivedMessage = event.messages[0];
-        if (!receivedMessage?.message?.extendedTextMessage) return;
+    // Respond only once to the user's selection (1-10) and not continuously
+    if (cmd === 'menu') {
+        Matrix.sendMessage(m.from, {
+            text: mainMenu,
+            contextInfo: { mentionedJid: [m.sender] }
+        });
+    }
 
-        const receivedText = receivedMessage.message.extendedTextMessage.text.trim();
-        if (receivedMessage.message.extendedTextMessage.contextInfo?.stanzaId !== sentMessage.key.id) return;
-
-        const menus = {
-            "1": `
+    // Define responses for each menu option
+    const menus = {
+        "1": `
 🔽 *DOWNLOAD MENU* 🔽
 📱 *apk* - Download APK files  
 🎵 *play* - Download songs  
@@ -101,7 +93,7 @@ Reply with *Menu* to start.
 🎧 *ytmp3* - Download YouTube MP3  
 📹 *ytmp4* - Download YouTube MP4
 `,
-            "2": `
+        "2": `
 🔽 *CONVERTER MENU* 🔽
 💬 *attp* - Text to Audio  
 🔢 *ebinary* - Encode to Binary  
@@ -110,7 +102,7 @@ Reply with *Menu* to start.
 🎵 *mp3* - Convert to MP3  
 🔗 *url* - URL Shortener
 `,
-            "3": `
+        "3": `
 🔽 *AI MENU* 🔽
 🧠 *ai* - Access AI Features  
 🤖 *sheng on/off* - Toggle Sheng Language  
@@ -120,7 +112,7 @@ Reply with *Menu* to start.
 🧠 *gemini* - Use Gemini AI  
 📖 *define* - Define a word or term  
 `,
-            "4": `
+        "4": `
 🔽 *TOOLS MENU* 🔽
 🧮 *calculator* - Simple Calculator  
 📧 *tempmail* - Temporary Email Service  
@@ -131,7 +123,7 @@ Reply with *Menu* to start.
 🌐 *shorten* - URL Shortener  
 💾 *save* - Save Content for Later
 `,
-            "5": `
+        "5": `
 🔽 *GROUP MENU* 🔽
 📋 *groupinfo* - Get Group Information  
 🚫 *hidetag* - Hide Group Tag  
@@ -149,7 +141,7 @@ Reply with *Menu* to start.
 🗳️ *poll* - Create a Poll  
 📱 *vcf* - Share Contact in VCF format
 `,
-            "6": `
+        "6": `
 🔽 *SEARCH MENU* 🔽
 🎵 *play* - Search Songs  
 🔍 *yts* - Search YouTube  
@@ -162,7 +154,7 @@ Reply with *Menu* to start.
 📖 *bible* - Search Bible Verses  
 📖 *biblebooks* - List Bible Books
 `,
-            "7": `
+        "7": `
 🔽 *MAIN MENU* 🔽
 🏓 *ping* - Ping the Bot  
 ⚡ *alive* - Check if Bot is Alive  
@@ -171,7 +163,7 @@ Reply with *Menu* to start.
 💬 *about* - About the Bot  
 🔗 *repo* - Get Bot Repository Links
 `,
-            "8": `
+        "8": `
 🔽 *OWNER MENU* 🔽
 🚪 *join* - Join Group  
 👋 *leave* - Leave Group  
@@ -191,13 +183,13 @@ Reply with *Menu* to start.
 🧹 *del* - Delete Messages  
 🔗 *antidelete on/off* - Anti-delete Protection
 `,
-            "9": `
+        "9": `
 🔽 *STALK MENU* 🔽
 📞 *truecaller* - Lookup Truecaller Info  
 📷 *instastalk* - Instagram Stalk  
 💻 *githubstalk* - GitHub User Stalk  
 `,
-            "10": `
+        "10": `
 🔽 *LOGO MENU* 🔽
 🖼️ *logo* - Create a Logo  
 🎮 *hacker* - Hacker Style Logo  
@@ -244,16 +236,15 @@ Reply with *Menu* to start.
 💰 *gold* - Gold Style Logo  
 ✨ *glow* - Glow Effect
 `
-        };
+    };
 
-        // If a valid menu option is selected, respond with that specific menu
-        if (menus[receivedText]) {
-            Matrix.sendMessage(m.from, {
-                text: menus[receivedText],
-                contextInfo: { mentionedJid: [m.sender] }
-            });
-        }
-    });
+    // If a valid menu option is selected, respond with that specific menu
+    if (menus[cmd]) {
+        Matrix.sendMessage(m.from, {
+            text: menus[cmd],
+            contextInfo: { mentionedJid: [m.sender] }
+        });
+    }
 };
 
 export default menu;
