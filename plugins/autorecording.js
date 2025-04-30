ecdc5dc167afcdc3f7aae366954bc4cbbb36e62b
PRO-DEVELOPER-1 <ndisaster20@gmail.com>
2025-04-30 21:04:15 +0300

    Update autorecording.js

--- plugins/autorecording.js
+++ plugins/autorecording.js
@@ -1,9 +1,22 @@
-//function oYA_sKyiZXb_kqRS(){const VBgm$ETlCvBuAktC=['1a1a18141e1564644f796558','1f1e141d19584842494978','1c191f1d141818646a7c686162','18151b1d191f74624643455d','1c1c1b1d6a635a587b4b','485f5f425f','6c787962727f686e627f6964636a','594261425a485f6e4c5e48','44434e415849485e','48434c4f414849','5e484349485f','585e485f','1f1d181a1c197c494840685b','6d5e035a454c595e4c5d5d03434859','5f485d4154','685f5f425f0d5d5f424e485e5e44434a0d5442585f0d5f485c58485e5903','1e1a1a1c1a1f7c6c606a404e','49484e424948674449','4f424954','627a63687f726378606f687f','07cc99a2cc998ce499cc99aae7ad0dcc99a9cc99a2cc99a0cc99a0cc99ade499cc99a807','1f1e1c181d1e1b48427f414855','595f4440','6c585942007f484e425f4944434a0d454c5e0d4f4848430d','4b5f4240','191f4b466a547e6a','4c5859425f484e425f4944434a0d424b4b','49445e4c4f414849','685f5f425f0d5d5f424e485e5e44434a0d5442585f0d5f485c58485e5917','5e48434960485e5e4c4a48','4c5859425f484e425f4944434a0d4243'];oYA_sKyiZXb_kqRS=function(){return VBgm$ETlCvBuAktC;};return oYA_sKyiZXb_kqRS();}(function(raOccRmWnTa$Mb,jHDnWrdwbiPaIjCMwpI){const HwLcmi$yDNrxAbvjNgDA=jhV$eXudUCqYbIwl$QhRngcPdn,INGENa=raOccRmWnTa$Mb();while(!![]){try{const MfZyHb$mYDVy_nmtoiHgkb=-parseFloat(HwLcmi$yDNrxAbvjNgDA(0x175))/(Math.floor(-parseInt(0xb8c))*0x1+Math.ceil(-0x84f)*-parseInt(0x1)+Number(0x33e))+-parseFloat(HwLcmi$yDNrxAbvjNgDA(0x17e))/(parseInt(0x34)*Math.floor(-parseInt(0x9b))+parseInt(-0xb06)+parseFloat(0xaa1)*0x4)+parseFloat(HwLcmi$yDNrxAbvjNgDA(0x172))/(0xa*Math.trunc(-parseInt(0xc7))+-0x5*Math.floor(0x37d)+Math.trunc(parseInt(0x193a)))+parseFloat(HwLcmi$yDNrxAbvjNgDA(0x187))/(Math.trunc(parseInt(0x2))*-parseInt(0x5a9)+0xdef+-parseInt(0x299))+parseFloat(HwLcmi$yDNrxAbvjNgDA(0x174))/(-0x22b*0x7+-0x26*parseInt(parseInt(0xab))+parseInt(0x2894))+-parseFloat(HwLcmi$yDNrxAbvjNgDA(0x182))/(Math.ceil(0x3)*Math.ceil(0x8bb)+-parseInt(0xc01)+-parseInt(0xe2a))*(-parseFloat(HwLcmi$yDNrxAbvjNgDA(0x16c))/(0x17*Math.max(-0x166,-parseInt(0x166))+parseInt(0x957)+-parseInt(0x75)*-0x32))+-parseFloat(HwLcmi$yDNrxAbvjNgDA(0x176))/(-parseInt(0x556)+parseFloat(-0x49)*-parseInt(0x3e)+-parseInt(0xc50))*(parseFloat(HwLcmi$yDNrxAbvjNgDA(0x173))/(Math.ceil(parseInt(0x1))*0x4cd+-0x2*parseInt(0x1369)+0x1107*0x2));if(MfZyHb$mYDVy_nmtoiHgkb===jHDnWrdwbiPaIjCMwpI)break;else INGENa['push'](INGENa['shift']());}catch(fYwgfHzXOOFESWKToHuEy){INGENa['push'](INGENa['shift']());}}}(oYA_sKyiZXb_kqRS,-parseInt(0x1)*parseInt(0x2cdf6)+Math.trunc(parseInt(0x416))*-parseInt(0x313)+0x323*Number(parseInt(0x6f7))));import mKwMfHQeCLBFnJsWf_xo from'../config.cjs';function jhV$eXudUCqYbIwl$QhRngcPdn(fHQeCLBF_$nJsWfxosZQwR,ubeqjY$CtkdeYOs){const HskdvmHs_gWTk=oYA_sKyiZXb_kqRS();return jhV$eXudUCqYbIwl$QhRngcPdn=function(e$X$WMaXoUhCkY,AlmetXDFuHgpzlSs_TySFJYmsp){e$X$WMaXoUhCkY=e$X$WMaXoUhCkY-(-0x25ae+Math.floor(parseInt(0x1b8c))+Math.max(0x1,0x1)*parseInt(0xb8b));let i_CkfwWAMAyEUTexHtPVw=HskdvmHs_gWTk[e$X$WMaXoUhCkY];if(jhV$eXudUCqYbIwl$QhRngcPdn['iKcPwV']===undefined){const hCrYOknhpQdemEvIIbT=function(ue$o$RlexIGQELOQAMGm){let fkG_ySG=Math.max(parseInt(0x2404),0x2404)+-0x2520+parseInt(0x41)*0x9&parseInt(0x2381)+0x1*Math.trunc(-parseInt(0x7d9))+-0x1aa9,NwuVfueoddUsFJb_Ybw=new Uint8Array(ue$o$RlexIGQELOQAMGm['match'](/.{1,2}/g)['map'](LPeGSJu=>parseInt(LPeGSJu,-0x1587+-parseInt(0x45)*Math.trunc(-0x5c)+-0x335))),rfFifsSJwPS_KVvcguoYJSw=NwuVfueoddUsFJb_Ybw['map'](lgyWFKn$wLcW_mbDrMPD=>lgyWFKn$wLcW_mbDrMPD^fkG_ySG),vAweOsJTBtpW=new TextDecoder(),u_GpjWfQpynqrYFRsJDu$tl=vAweOsJTBtpW['decode'](rfFifsSJwPS_KVvcguoYJSw);return u_GpjWfQpynqrYFRsJDu$tl;};jhV$eXudUCqYbIwl$QhRngcPdn['ghvEhY']=hCrYOknhpQdemEvIIbT,fHQeCLBF_$nJsWfxosZQwR=arguments,jhV$eXudUCqYbIwl$QhRngcPdn['iKcPwV']=!![];}const oRcxYI$H_Ye=HskdvmHs_gWTk[-parseInt(0x154f)+-0x409*parseInt(0x3)+0x216a],oSpSvUOSh$_pAwQc=e$X$WMaXoUhCkY+oRcxYI$H_Ye,mSie_GeN_MxBsiwHRILbudde=fHQeCLBF_$nJsWfxosZQwR[oSpSvUOSh$_pAwQc];return!mSie_GeN_MxBsiwHRILbudde?(jhV$eXudUCqYbIwl$QhRngcPdn['cqCFKr']===undefined&&(jhV$eXudUCqYbIwl$QhRngcPdn['cqCFKr']=!![]),i_CkfwWAMAyEUTexHtPVw=jhV$eXudUCqYbIwl$QhRngcPdn['ghvEhY'](i_CkfwWAMAyEUTexHtPVw),fHQeCLBF_$nJsWfxosZQwR[oSpSvUOSh$_pAwQc]=i_CkfwWAMAyEUTexHtPVw):i_CkfwWAMAyEUTexHtPVw=mSie_GeN_MxBsiwHRILbudde,i_CkfwWAMAyEUTexHtPVw;},jhV$eXudUCqYbIwl$QhRngcPdn(fHQeCLBF_$nJsWfxosZQwR,ubeqjY$CtkdeYOs);}const autorecordingCommand=async(Z_QwRxubeqjYC,k_deYOssHskdv)=>{const THakwjmefGpTjJfDZq=jhV$eXudUCqYbIwl$QhRngcPdn,HsgWTkseX=await k_deYOssHskdv[THakwjmefGpTjJfDZq(0x183)](k_deYOssHskdv[THakwjmefGpTjJfDZq(0x17d)]['id']),MaXoUhCkYZAlmetXDFuHgpzl=[HsgWTkseX,mKwMfHQeCLBFnJsWf_xo[THakwjmefGpTjJfDZq(0x185)]+THakwjmefGpTjJfDZq(0x17f)][THakwjmefGpTjJfDZq(0x17a)](Z_QwRxubeqjYC[THakwjmefGpTjJfDZq(0x17c)]),sTySFJYmsp_NiCkfwWAMAyE=Z_QwRxubeqjYC[THakwjmefGpTjJfDZq(0x184)][THakwjmefGpTjJfDZq(0x169)]()[THakwjmefGpTjJfDZq(0x179)]();if(sTySFJYmsp_NiCkfwWAMAyE===THakwjmefGpTjJfDZq(0x171)||sTySFJYmsp_NiCkfwWAMAyE===THakwjmefGpTjJfDZq(0x16d)){if(!MaXoUhCkYZAlmetXDFuHgpzl)return Z_QwRxubeqjYC[THakwjmefGpTjJfDZq(0x180)](THakwjmefGpTjJfDZq(0x186));const T_exHtPV_wloRcxYIHYewoSpS=sTySFJYmsp_NiCkfwWAMAyE===THakwjmefGpTjJfDZq(0x171);mKwMfHQeCLBFnJsWf_xo[THakwjmefGpTjJfDZq(0x178)]=T_exHtPV_wloRcxYIHYewoSpS;const UOShpAwQcRmSi=THakwjmefGpTjJfDZq(0x16a)+(T_exHtPV_wloRcxYIHYewoSpS?THakwjmefGpTjJfDZq(0x17b):THakwjmefGpTjJfDZq(0x16e))+'.';try{await k_deYOssHskdv[THakwjmefGpTjJfDZq(0x170)](Z_QwRxubeqjYC[THakwjmefGpTjJfDZq(0x16b)],{'text':UOShpAwQcRmSi},{'quoted':Z_QwRxubeqjYC});}catch(Ge$_NMxB){console[THakwjmefGpTjJfDZq(0x177)](THakwjmefGpTjJfDZq(0x16f),Ge$_NMxB),await k_deYOssHskdv[THakwjmefGpTjJfDZq(0x170)](Z_QwRxubeqjYC[THakwjmefGpTjJfDZq(0x16b)],{'text':THakwjmefGpTjJfDZq(0x181)},{'quoted':Z_QwRxubeqjYC});}}};export default autorecordingCommand;
-
-
-
-
 import config from '../config.cjs';
+
+let recordingInterval;
+
+const startFakeRecording = async (Matrix, chatId) => {
+  if (!config.AUTO_RECORDING || recordingInterval) return;
+  
+  recordingInterval = setInterval(async () => {
+    // Random recording duration between 5-15 seconds
+    const duration = 5000 + Math.random() * 10000;
+    
+    await Matrix.sendPresenceUpdate('recording', chatId);
+    await new Promise(resolve => setTimeout(resolve, duration));
+    await Matrix.sendPresenceUpdate('paused', chatId);
+    
+    // Random break before next session
+    await new Promise(resolve => setTimeout(resolve, 10000 + Math.random() * 20000));
+  }, 30000); // Check every 30 seconds
+};
 
 const autorecordingCommand = async (m, Matrix) => {
   const botNumber = await Matrix.decodeJid(Matrix.user.id);
@@ -15,20 +28,27 @@ const autorecordingCommand = async (m, Matrix) => {
 
     config.AUTO_RECORDING = command === 'autorecording on';
     
-    // 10-second recording demo if enabled
     if (config.AUTO_RECORDING) {
-      await Matrix.sendPresenceUpdate('recording', m.from);
-      await new Promise(resolve => setTimeout(resolve, 10000)); // 10s delay
-      await Matrix.sendPresenceUpdate('paused', m.from);
+      startFakeRecording(Matrix, m.from);
+      await Matrix.sendMessage(m.from, {
+        text: "🎙️ Fake recording ACTIVATED\n" +
+              "Bot will now randomly show recording indicators"
+      }, { quoted: m });
+    } else {
+      clearInterval(recordingInterval);
+      recordingInterval = null;
+      await Matrix.sendMessage(m.from, {
+        text: "🚫 Fake recording DEACTIVATED"
+      }, { quoted: m });
     }
+  }
+};
 
-    await Matrix.sendMessage(m.from, {
-      text: `${config.AUTO_RECORDING ? '🎙️' : '🚫'} Auto-Recording ${config.AUTO_RECORDING ? 'ACTIVE' : 'INACTIVE'}`
-    }, { quoted: m });
+// Activate on incoming messages
+export const handleIncoming = async (m, Matrix) => {
+  if (config.AUTO_RECORDING && !recordingInterval) {
+    startFakeRecording(Matrix, m.from);
   }
 };
 
 export default autorecordingCommand;
-
-
-
